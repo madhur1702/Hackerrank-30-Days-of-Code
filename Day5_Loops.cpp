d7b26362aaf12fc74c445157415bdea817d5a1cb
Madhur Bhandarkar <150015788+madhur1702@users.noreply.github.com>
2024-08-30 20:02:59 +0530

    Create Day5_Loops.cpp

---
+++ Day5_Loops.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main()
+{
+    int n;
+    cin>>n;
+    for(int i=1;i<=10;i++)
+    {
+        int j=n*i;
+        cout<<n<<" x "<<i<<" = "<<j<<endl;
+    }
+
+    return 0;
+}
+
+
