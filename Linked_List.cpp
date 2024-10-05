77090c222ef418c02442e5ea0b976f1b52151ac4
Madhur Bhandarkar <150015788+madhur1702@users.noreply.github.com>
2024-10-05 23:22:12 +0530

    Create Linked_List.cpp

---
+++ Linked_List.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <cstddef>
+using namespace std;	
+class Node
+{
+    public:
+        int data;
+        Node *next;
+        Node(int d){
+            data=d;
+            next=NULL;
+        }
+};
+class Solution{
+    public:
+
+      Node* insert(Node *head,int data)
+      {
+          //Complete this method
+          Node* ptr = new Node(data);
+          Node* p=head;
+          if (p == NULL)
+        {
+            return ptr;
+        }
+        while (p->next != NULL)
+        {
+            p = p->next;
+        }
+        p->next = ptr;
+
+        return head;
+
+      }
+
+      void display(Node *head)
+      {
+          Node *start=head;
+          while(start)
+          {
+              cout<<start->data<<" ";
+              start=start->next;
+          }
+      }
+};
+int main()
+{
+	Node* head=NULL;
+  	Solution mylist;
+    int T,data;
+    cin>>T;
+    while(T-->0){
+        cin>>data;
+        head=mylist.insert(head,data);
+    }	
+	mylist.display(head);
+		
+}
